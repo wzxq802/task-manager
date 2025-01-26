79ac0148af7c05392e5e0289d7b69e80b4e2d751
Ulyana <ulyana20011119@gmail.com>
2025-01-26 19:42:31 +0300

    added board page

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,16 @@
+import {startCase} from "lodash"
 import { OrgControl } from "./_components/org-control";
+import { auth } from "@clerk/nextjs/server";
+
+export async function generateMetadata() {
+    const authData = await auth();
+    const orgSlug = authData.orgSlug || "Organization"; 
+
+    return {
+        title: startCase(orgSlug || "Organization")
+    }
+}
+
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
