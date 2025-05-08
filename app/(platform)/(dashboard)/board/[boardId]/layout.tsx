aba7f51a05e4366a2ff4f89bfbca4c77de47fc31
Ulyana <ulyana20011119@gmail.com>
2025-05-08 13:41:23 +0300

    changed boardId

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -2,33 +2,9 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { notFound, redirect } from "next/navigation";
 import { BoardNavbar } from "./_components/board-navbar";
-import { ReactNode } from "react"; 
+import { ReactNode } from "react";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { boardId: string };
-}) {
-  const authData = await auth();
-  const orgId = authData.orgId;
-
-  if (!orgId) {
-    return { title: "Board" };
-  }
-
-  const board = await db.board.findUnique({
-    where: {
-      id: params.boardId,
-      orgId,
-    },
-  });
-
-  return {
-    title: board?.title || "Board",
-  };
-}
-
-// ✅ Задаём тип явно
+// Типы для layout компонента Next.js
 interface BoardIdLayoutProps {
   children: ReactNode;
   params: {
