52c37f87fc0e1dbfd01255ecb4c1986d0da80126
Ulyana <ulyana20011119@gmail.com>
2025-05-08 15:25:24 +0300

    changed api-cards-route.tsx

--- app/api/cards/[cardId]/logs/route.ts
+++ app/api/cards/[cardId]/logs/route.ts
@@ -1,24 +1,23 @@
 import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { ENTITY_TYPE } from "@prisma/client";
 
-export async function GET(
-    request: Request,
-    { params } : {params: { cardId: string } }
-
-){
+export async function GET(request: NextRequest) {
     try {
-        const { userId, orgId} = await auth();
+        const { userId, orgId } = await auth();
 
         if (!userId || !orgId) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
 
+        // Получаем cardId из URL
+        const cardId = request.nextUrl.pathname.split("/")[4]; // ["", "api", "cards", "{cardId}", "logs"]
+
         const auditLogs = await db.auditLog.findMany({
             where: {
                 orgId,
-                entityId: params.cardId,
+                entityId: cardId,
                 entityType: ENTITY_TYPE.CARD,
             },
             orderBy: {
@@ -31,4 +30,4 @@ export async function GET(
     } catch {
         return new NextResponse("Internal error", { status: 500 });
     }
-};
+}
