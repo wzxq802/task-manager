89cb087e7d51c2e998066e37c695b96041f51387
Ulyana <ulyana20011119@gmail.com>
2025-05-08 17:31:23 +0300

    tsconf

--- app/api/cards/[cardId]/logs/route.ts
+++ app/api/cards/[cardId]/logs/route.ts
@@ -3,32 +3,31 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { ENTITY_TYPE } from "@prisma/client";
 
-export async function GET(request: NextRequest, { params }: { params: Record<string, string> }) {
-  
-  try {
+export async function GET(request: NextRequest, { params }: { params: { cardId: string } }) {
+    try {
       const { userId, orgId } = await auth();
-
+  
       if (!userId || !orgId) {
-          return new NextResponse("Unauthorized", { status: 401 });
+        return new NextResponse("Unauthorized", { status: 401 });
       }
-
+  
       const { cardId } = params; 
-
+  
       const auditLogs = await db.auditLog.findMany({
-          where: {
-              orgId,
-              entityId: cardId,
-              entityType: ENTITY_TYPE.CARD,
-          },
-          orderBy: {
-              createdAt: "desc",
-          },
-          take: 3,
+        where: {
+          orgId,
+          entityId: cardId,
+          entityType: ENTITY_TYPE.CARD,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: 3,
       });
-
+  
       return NextResponse.json(auditLogs);
-  } catch (error) {
+    } catch (error) {
       console.error(error); 
       return new NextResponse("Internal error", { status: 500 });
+    }
   }
-}
