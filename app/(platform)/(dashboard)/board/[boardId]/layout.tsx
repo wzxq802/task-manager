cd99dfe6241b56f144431a7a3387be7450de1828
Ulyana <ulyana20011119@gmail.com>
2025-05-08 15:18:05 +0300

    changed packege.json

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -4,27 +4,25 @@ import { auth } from "@clerk/nextjs/server";
 import { notFound, redirect } from "next/navigation";
 import { BoardNavbar } from "./_components/board-navbar";
 
-// Определим тип Props с параметрами динамической страницы
+// Тип с params как Promise
 type Props = {
   children: ReactNode;
-  params: {
+  params: Promise<{
     boardId: string;
-  };
+  }>;
 };
 
-// Основной компонент
 export default async function BoardIdLayout({ children, params }: Props) {
-  const { boardId } = params; // получаем идентификатор доски
+  const { boardId } = await params; // теперь корректно обрабатываем Promise
 
   try {
-    const authData = await auth(); // выполняем авторизацию
-    const orgId = authData?.orgId; // получаем идентификатор организации
+    const authData = await auth();
+    const orgId = authData?.orgId;
 
     if (!orgId) {
-      redirect("/select-org"); // переадресовываемся, если нет выбранной организации
+      redirect("/select-org");
     }
 
-    // проверяем существование доски
     const board = await db.board.findUnique({
       where: {
         id: boardId,
@@ -33,10 +31,9 @@ export default async function BoardIdLayout({ children, params }: Props) {
     });
 
     if (!board) {
-      return notFound(); // возвращаем 404 страницу, если доска не найдена
+      return notFound();
     }
 
-    // рендерим разметку страницы
     return (
       <div
         className="relative h-full bg-no-repeat bg-cover bg-center"
@@ -52,7 +49,7 @@ export default async function BoardIdLayout({ children, params }: Props) {
       </div>
     );
   } catch (err) {
-    console.error("Error fetching board:", err); // регистрируем ошибку
-    throw new Error("An unexpected error occurred while loading the board."); // выбрасываем исключение
+    console.error("Error fetching board:", err);
+    throw new Error("An unexpected error occurred while loading the board.");
   }
 }
